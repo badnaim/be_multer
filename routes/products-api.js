310d43023e588d2b13abe6f87e78d9ddb6b1f1f3
badnaim <batnyaam.tumurbat@gmail.com>
2023-03-20 12:43:55 +0800

    b

--- routes/products-api.js
+++ routes/products-api.js
@@ -5,15 +5,22 @@ import { nanoid } from "nanoid";
 
 const productRouter = express.Router();
 
-productRouter.get("/", async (request, response) => {
-  console.log("get request 1");
-  const getProduct = await Product.find({});
-  try {
-    response.status(200).send(getProduct);
-    console.log(getProduct);
-  } catch (error) {
-    response.status(404).send(error);
-  }
+// productRouter.get("/", async (request, response) => {
+//   console.log("get request 1");
+//   const getProduct = await Product.find({});
+//   try {
+//     response.status(200).send(getProduct);
+//     console.log(getProduct);
+//   } catch (error) {
+//     response.status(404).send(error);
+//   }
+// });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/tmp");
+  },
+  filename: (req, file, cb) => {},
 });
 
 // productRouter.post("/post", async (req, res) => {
